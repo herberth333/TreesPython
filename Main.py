b34d51b2e966c996f8c6958137331f4ed0529886
Herbert Hardman <herberthardmanf@gmail.com>
2025-05-28 12:23:42 -0300

    feat: criei a parte inorder, falta apenas ajustes para funcionar

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@ from models.Node import Node
                     
 # Exemplo de uso:
 raiz = Node(8)
-nosFilhos = [3, 10, 1, 6, 14, 4, 7, 13]
+nosFilhos = [8, 9, 24, 55, 12, 3, 4, 13]
 
 for i in nosFilhos:
     raiz.insert(i)
 
-raiz.visualizar_arvore()
+# raiz.visualizar_arvore()
+raiz.visualizar_inorder()
--- models/Node.py
+++ models/Node.py
@@ -5,7 +5,7 @@ class Node:
         self.right = None
         self.data = data
 
-#Função de inserir na arvove os dados
+    #Função de inserir na arvove os dados
     def insert(self, data):
         #Verifica se a Raiz tem alguma coisa
         if self.data is None:
@@ -25,12 +25,43 @@ class Node:
                 else:
                     self.right.insert(data)
     
+    def inOrder(root):
+        if root is None:
+            return
+        else:    
+            inOrder(root.left)
+            print(root.data)
+            inOrder(root.right)
+    
+    
     # Funções para gerar imagem da árvore, utiliza a biblioteca graphviz para gerar a imagem
     def visualizar_arvore(self, filename="arvore_bst"):
         dot = Digraph()
         self._adicionar_nos(dot, self)
         dot.render(filename, format='png', cleanup=True)
         print(f"Árvore renderizada em: {filename}.png")
+    
+    def visualizar_inorder(self, filename="inorder_percorrido"):
+        ordem = []
+
+        def in_order(node):
+            if node is None:
+                return
+            in_order(node.left)
+            ordem.append(node.data)
+            in_order(node.right)
+
+        in_order(self)
+
+        dot = Digraph(comment="Percurso InOrder")
+        for i, val in enumerate(ordem):
+            dot.node(str(i), str(val))
+            if i > 0:
+                dot.edge(str(i - 1), str(i))
+
+        dot.render(filename, format='png', cleanup=True)
+        print(f"Percurso inOrder renderizado em: {filename}.png")
+    
     def _adicionar_nos(self, dot, node):
         if node is not None:
             dot.node(str(id(node)), str(node.data))
